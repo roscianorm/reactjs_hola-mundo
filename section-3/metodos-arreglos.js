7c797e3fc14103bb8afe63f74a2ad0ac98d43300
Miguel Rosciano <roscianorm@gmail.com>
2021-09-30 11:14:31 -0600

    array.map init

--- section-3/metodos-arreglos.js
+++ section-3/metodos-arreglos.js
@@ -1,1 +1,7 @@
-const arr = [0, 1, 2, 3, 4]
+const arr = [11, 22, 33, 44, 55]
+
+const r = arr.filter((el, index) => {
+	console.log('index', index, 'valor =', el)
+	return el < 33
+})
+console.log(r)
